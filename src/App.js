6379b7126878b6e04c0066aa0ca0a90325d31616
chetana <chetanadharpure@gmail.com>
2023-06-13 11:50:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useEffect, useState } from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import axios from "axios";
+
 
 function App() {
+ const Array=["Delhi","Manali","Nagpur","Mumbai","Pune","Hyderabad"]
+ const[data,setData]=useState([])
+ useEffect(()=>{
+            console.log("hii")
+            Array.map((item)=>{
+     return axios.get(`https://api.weatherapi.com/v1/current.json?key=09976f826ef1418eba1143300231206&q=${item}&aqi=no`).then((res)=>{
+            console.log("output",res.data.current.temp_c)
+            data.push({"city":item ,
+                      "temp" :res.data.current.temp_c
+                      })
+            setData([...data]) 
+            console.log("data",data)
+      })
+ })
+},[])
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <>
+       <div style={{padding:"100px",
+                    backgroundColor: "#282c34",
+                    minHeight: "100vh"}} >
+                <Row xs={1} md={3} className="g-6">
+                {data.map((item, idx) => (
+  
+            <Col key={idx}>
+            <Card style={{padding : "10px", margin:"10px"}}>
+ 
+            <Card.Body>
+            <Card.Title style={{textDecoration:"underline"}}> &#9729;  {item.city}</Card.Title>
+            <Card.Text>{item.temp}° celsius
+     
+            </Card.Text>
+            </Card.Body>
+            </Card>
+            </Col>
+))}
+            </Row>
+</div> </>
+    );}
 
 export default App;
